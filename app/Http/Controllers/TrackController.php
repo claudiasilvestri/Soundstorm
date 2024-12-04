5b4e94fbe0899ef95a3e9d9e602426387836f332
Nicola Menonna <nicola.menonna@aulab.it>
2024-12-04 15:03:11 +0100

    fixed error download track

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -57,7 +57,7 @@ class TrackController extends Controller
 
         $track->genres()->attach($request->input('genres'));
 
-        return redirect()->route('homepage')->with('success', 'Hai aggiunto correttamente il tuo brano.');
+        return redirect()->route('welcome')->with('success', 'Hai aggiunto correttamente il tuo brano.');
     }
 
     public function show(Track $track)
@@ -150,6 +150,8 @@ class TrackController extends Controller
                 $username, $trackTitle
             ));
 
-        return Storage::download($track->path);
+        $path = storage_path('app/public/' . $track->path);
+        return response()->download($path);
+            
     }
 }
